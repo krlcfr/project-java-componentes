f5540703fbe07b0479e98f86a31f3074b813f4af
Santiago <santiago.gallego28@outlook.com>
2025-11-21 19:23:03 -0500

    feat: implementación de clase Vehiculo con atributos y métodos

---
+++ src/vehiculo.java
@@ -0,0 +1,40 @@
+//Vehiculo, identificación, marca, modelo, etc.
+public class Vehiculo {
+    //atributos de la clase vehiculo
+    private String marca;
+    private String modelo;
+    private String matricula;
+    private int anio;
+    //atributos de otras clases
+
+    //añadir metodo constructor
+    public Vehiculo(String marca, String modelo, String matricula, int anio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.anio = anio;
+    }
+
+    //Todo: geters
+    public String getMarca(){ return marca;}
+    public String getModelo(){ return modelo;}
+    public String getMatricula(){return matricula;}
+    public int getAnio(){return anio;}
+
+
+    //Todo: setters
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+    public void setAnio(int anio){
+        this.anio = anio;
+    }
+
+
+}
